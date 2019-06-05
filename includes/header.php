9ed1d75414213f2b7ea2e81a3973748de8e28ba5
jonathonkindle <jonathonkindle@gmail.com>
2019-06-05 16:28:26 -0700

    fixed the nav links

--- includes/header.php
+++ includes/header.php
@@ -21,11 +21,11 @@
                     <ul class="topnav" id="myTopnav">
                         <?=makeLinks($nav1)?>
                         <!-- <li><a href="/web120/index.php" class="selected">Welcome</a></li>
-                        <li><a href="/web120/big/index.php">Big</a></li>
-                        <li><a href="/web120/aia.php">AIA</a></li>
-                        <li><a href="/web120/flowchart.php">Flowchart</a></li>
-                        <li><a href="/web120/fp/index.php">Final Project</a></li>
-                        <li><a href="/web120/contactme.php">Contact Jonathon</a></li> -->
+                        <li><a href="/big/big-index.php">Big</a></li>
+                        <li><a href="/aia.php">AIA</a></li>
+                        <li><a href="/flowchart.php">Flowchart</a></li>
+                        <li><a href="/fp/fp-index.php">Final Project</a></li>
+                        <li><a href="/contactme.php">Contact Jonathon</a></li> -->
                         <li class="icon"> <a href="javascript:void(0);" onclick="myFunction()">&#9776;</a> </li>
                     </ul>
                 </nav>
