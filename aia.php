9d2475d961a1705431c9ebe191f4d95170d7ec5a
jonathonkindle <jonathonkindle@gmail.com>
2019-06-06 23:23:46 -0700

    finished aia page and added css to portal.css

--- aia.php
+++ aia.php
@@ -5,8 +5,8 @@
 <!-- START LEFT COL -->
 
    <h2 class="pageID"><?=$pageID?></h2> 
-   <h3>Client Information</h3>
-   <table>
+   <h3 class="aiah3">Client Information</h3>
+   <table id="aiaclientinfo">
       <tr>
          <td>Clients' full name and final decision maker</td>
          <td>Jonathon Kindle (This is a portfolio project)</td>
@@ -28,16 +28,86 @@
          <td>Portfolio project that leads to --> Presence on the Internet, Creating a new client base, Small business startup</td>
       </tr>
    </table>
-
+   <h3 class="aiah3">AIA</h3>
+   <table id="aiainfo">
+      <tr>
+         <td>Audience</td>
+         <td>Issues, Concerns, Needs</td>
+         <td>Approach</td>
+      </tr>
+      <tr>
+         <td>
+            <p>Students:</p>
+            <p>The age range of this audience will likely be primarily 19-30 years of age.</P>
+            <p>They will probably be using a variety of devices from pc & mac, to mobile phones.</p>
+         </td>
+         <td>
+            <ul>
+               <li>Easy navigation and user interactivity.</li>
+               <li>Is ther plenty of information and pictures of products?</li>
+               <li>SEO and maps placement for easy to find site and business location.</li>
+               <li>Is there clearly defined prices for rentals/services?</li>
+            </ul>
+         </td>
+         <td>
+            <ul>
+               <li>Create a clear, simple, responsive navigation that remains consistent on all pages.</li>
+               <li>Use a gallery that is interactive and can feaature captions when focused that include basic info and linkes to the products page.</li>
+               <li>Use clear meta info for searching and register the business location with major maps (google, apples' maps)</li>
+               <li>Easy to read and scroll through succint information on the services page.</li>
+            </ul>
+         </td>
+      </tr>
+      <tr>
+         <td>
+            <p>Cyclists:</p>
+            <p>The age range of this audience will vary from middle teens all the way to active elderly.</P>
+            <p>This audience will mostly use mobile phones as it is easy to carry on a ride.</p>
+         </td>
+         <td>
+            <ul>
+               <li>Easy navigation and user interactivity.</li>
+               <li>Clear contact information that is easy to get to.</li>
+               <li>SEO and maps placement for easy to find site and business location.</li>
+               <li>Can the site load on low bandwidth phone service connection (ie. 3g/4g)?</li>
+            </ul>
+         </td>
+         <td>
+            <ul>
+               <li>Create a clear, simple, responsive navigation that remains consistent on all pages.</li>
+               <li>Contact link is clearly identified in top navigation and footer.</li>
+               <li>Use clear meta info for searching and register the business location with major maps (google, apples' maps)</li>
+               <li>Optimize the site with an image gallery and scalable images for fast loading.</li>
+            </ul>
+         </td>
+      </tr>
+      <tr>
+         <td>
+            <p>Tourists:</p>
+            <p>The age range of this audience will vary from middle teens all the way to active elderly.</P>
+            <p>This audience will mostly use mobile phones and laptops to search for rental locations.</p>
+         </td>
+         <td>
+            <ul>
+               <li>Easy navigation and user interactivity.</li>
+               <li>Clear contact information that is easy to get to.</li>
+               <li>SEO and maps placement for easy to find site and business location.</li>
+               <li>Is there clearly defined prices for rentals/services?</li>
+            </ul>
+         </td>
+         <td>
+            <ul>
+               <li>Create a clear, simple, responsive navigation that remains consistent on all pages.</li>
+               <li>Contact link is clearly identified in top navigation and footer.</li>
+               <li>Use clear meta info for searching and register the business location with major maps (google, apples' maps)</li>
+               <li>Easy to read and scroll through succint information on the services page.</li>
+            </ul>
+         </td>
+      </tr>
+   </table>
 <!-- END LEFT COL -->
 
 <!-- START RIGHT COL -->
-
-   <h3>Provide 2 resource links to client surveys</h3>
-   <ul>
-      <li>this is a web client survey resource</li>
-      <li>this is a web client survey resource</li>
-   </ul>
 
 <!-- END RIGHT COL -->
 <?php include 'includes/footer.php';?>
